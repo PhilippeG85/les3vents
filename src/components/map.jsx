e08d3273387d1e70cecad08a8624bdda9383c230
Philippe Gonse <pgonse@gmail.com>
2021-02-02 10:11:57 -0500

    clean

---
+++ src/components/map.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+function MapContainer({ google, height, width }) {
+  const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
+
+  return (
+    <div style={{ position: "relative", height, width }}>
+      <Map
+        google={google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={{ lat: 46.489, lng: -1.769 }}
+      >
+        <Marker position={{ lat: 46.489394388586106, lng: -1.769921479286359 }} />
+      </Map>
+    </div>
+  );
+}
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+export default GoogleApiWrapper({
+  apiKey: API_KEY
+})(MapContainer);
