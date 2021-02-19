05ede3e552ecaf810cb6b31535b4375b49ac6a81
Philippe Gonse <pgonse@gmail.com>
2021-02-19 16:48:07 -0500

    close mobile navbar on click

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,12 +40,12 @@ function Navbar() {
         <h1 className="navbar-h1">Les Trois Mâts</h1>
         <FontAwesomeIcon icon={faBars} onClick={() => handleClick()} />
         <div className={`mobile-menu ${left ? "left" : ""}`}>
-          <Link to="/" className="navbar-btn">Accueil</Link>
-          <Link className="navbar-btn" to="/description">Descriptions & Photos</Link>
-          <Link to="/disponibilite&tarif" className="navbar-btn">Disponibilités & Tarifs</Link>
-          <Link to="/emplacement" className="navbar-btn">Emplacement & Contact</Link>
-          <Link to="/activite" className="navbar-btn">Activités</Link>
-          <Link to="/lien" className="navbar-btn">Lien</Link>
+          <Link onClick={() => handleClick()} to="/" className="navbar-btn">Accueil</Link>
+          <Link onClick={() => handleClick()} className="navbar-btn" to="/description">Descriptions & Photos</Link>
+          <Link onClick={() => handleClick()} to="/disponibilite&tarif" className="navbar-btn">Disponibilités & Tarifs</Link>
+          <Link onClick={() => handleClick()} to="/emplacement" className="navbar-btn">Emplacement & Contact</Link>
+          <Link onClick={() => handleClick()} to="/activite" className="navbar-btn">Activités</Link>
+          <Link onClick={() => handleClick()} to="/lien" className="navbar-btn">Lien</Link>
         </div>
       </div>
     </>
