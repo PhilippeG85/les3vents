cc404f39388f807b312e29824fb8d6b2b7604a42
Philippe Gonse <pgonse@gmail.com>
2021-02-19 23:44:57 -0500

    responsive footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,13 +5,25 @@ function Footer() {
     <div className="footer">
       <div className="container footer-content">
         <h1>Les Trois Mâts</h1>
-        <div>
-          <p>22 Promenade Georges Godet</p>
-          <p>85100 Les Sables d&apos;Olonne</p>
+        <div className="footer-laptop">
+          <div>
+            <p>22 Promenade Georges Godet</p>
+            <p>85100 Les Sables d&apos;Olonne</p>
+          </div>
+          <div>
+            <p>Tel: 06-50-72-86-32</p>
+            <p>Email: elisabeth.gonse@gmail.com</p>
+          </div>
         </div>
-        <div>
-          <p>Tel: 06-50-72-86-32</p>
-          <p>Email: elisabeth.gonse@gmail.com</p>
+        <div className="footer-mobile">
+          <div style={{ paddingBottom: "6px" }}>
+            <p>22 Promenade Georges Godet</p>
+            <p>85100 Les Sables d&apos;Olonne</p>
+          </div>
+          <div>
+            <p>Tel: 06-50-72-86-32</p>
+            <p>Email: elisabeth.gonse@gmail.com</p>
+          </div>
         </div>
       </div>
       <p className="legal-footer">SARL ELJA & fils - RCS La Roche Sur Yon - 879 767 812 00016</p>
