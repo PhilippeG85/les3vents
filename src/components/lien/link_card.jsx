49683428a81b4268a86f7242dfd41717137f243b
Philippe Gonse <pgonse@gmail.com>
2021-02-20 09:19:12 -0500

    fix link on click and animation on mobile

--- src/components/lien/link_card.jsx
+++ src/components/lien/link_card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LinkCard({ number, title, link }) {
   return (
     <div className="link-card-content">
-      <a href={link} className="link-btn">
+      <a target="_blank" rel="noreferrer" href={link} className="link-btn">
         <div className="center link-card">
           <div className="flex-link">
             <h1 className="link-title">{number}</h1>
