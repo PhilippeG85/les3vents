46f8b76aaa1e488ed9027627fb37864e1e4566d7
Philippe Gonse <pgonse@gmail.com>
2021-05-11 20:35:23 -0400

    clear link

--- src/components/mapBox2.jsx
+++ src/components/mapBox2.jsx
@@ -10,7 +10,7 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
 export default function MapBox2() {
     const mapContainer = useRef(null);
 
-    const popupText = (`<h4><a target='_blank' href='https://www.google.com/maps/place/Appartement+Les+Trois+Mats/@46.4877614,-1.7729705,16.19z/data=!4m8!1m2!2m1!1sappartement+les+trois+mats+les+sables!3m4!1s0x4804674fab1351dd:0xebe554e0a646e82!8m2!3d46.4894551!4d-1.769861'>Les Trois Mats</a></h4><br /><p>22 Promenade Georges Godet<br />85100 Les Sables d&apos;Olonne</p><p>Tel: 06-50-72-86-32</p><p>Email: <a href='mailto:elisabeth.gonse@gmail.com'>elisabeth.gonse@gmail.com</a></p>`);
+    const popupText = (`<h4>Les Trois Mats</h4><br /><p>22 Promenade Georges Godet<br />85100 Les Sables d&apos;Olonne</p><p>Tel: 06-50-72-86-32</p><p>Email: <a href='mailto:elisabeth.gonse@gmail.com'>elisabeth.gonse@gmail.com</a></p>`);
 
     useEffect(() => {
         const map = new mapboxgl.Map({
